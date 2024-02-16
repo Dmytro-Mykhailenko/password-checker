575f826b34300d462dc5a060b27d33ccd0dd63fb
Dmytro-Mykhailenko <daemonkt@gmail.com>
2024-02-16 18:04:58 +0200

    Homework #7. New check-list, added tests

--- src/test/java/org/password/PasswordCheckerTest.java
+++ src/test/java/org/password/PasswordCheckerTest.java
@@ -1,12 +1,38 @@
 package org.password;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PasswordCheckerTest {
 
     @Test
-    void shouldReturnTrueForComplexPasswordWithDigitsAndSpecialChars() {
-         assertTrue( PasswordChecker.checkPasswordComplexity("Password123!") );
+    void checkTheCorrectPasswordIfItHas12Chars() {
+        assertTrue( PasswordChecker.checkPasswordComplexity("Password123!") );
     }
+
+    @Test
+    void checkTheCorrectPasswordIfItHas8Chars() {
+        assertTrue( PasswordChecker.checkPasswordComplexity("Pass456?") );
+    }
+
+    @Test
+    void checkPasswordIfItHas7CharsWithDigitsAndSpecialChars() {
+        assertFalse( PasswordChecker.checkPasswordComplexity("Pas789@") );
+    }
+
+    @Test
+    void checkPasswordIfItHas12CharsWithoutDigits() {
+        assertFalse( PasswordChecker.checkPasswordComplexity("Password?@*!") );
+    }
+
+    @Test
+    void checkPasswordIfItHas12CharsWithoutSpecialChars() {
+        assertFalse( PasswordChecker.checkPasswordComplexity("Password9876") );
+    }
+
+    @Test
+    void checkPasswordIfItEmpty() {
+        assertFalse( PasswordChecker.checkPasswordComplexity("") );
+    }
+
 }
