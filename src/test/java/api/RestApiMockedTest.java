02252e3a72c30a0e5b2e7c129e0bc49ceb4c5792
Dmytro-Mykhailenko <daemonkt@gmail.com>
2024-03-03 10:49:06 +0200

    Homework-#10, Updated API tests(parametrized tests). Added tests for /Create an order endpoint. Added performance test

--- src/test/java/api/RestApiMockedTest.java
+++ src/test/java/api/RestApiMockedTest.java
@@ -1,84 +1,34 @@
 package api;
 
-//import com.google.gson.JsonObject;
-
-import io.restassured.response.Response;
-import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
-
 import static io.restassured.RestAssured.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class RestApiMockedTest {
 
+    private final int[] VALID_ID = {1, 2, 5, 9, 10};
+    private final int[] INVALID_ID = {-1, 0, 11, 12, 20};
     private final String VALID_API_KEY = "1234567890123456";
+    private final String[] INVALID_API_KEY = {"", "3", "67594063", "123456789012345",
+            "qwertyuiopasdfgh", "5w2r4y7i3p1s6f8h", "$&*()_+-=|:<>'`~", "123456789.098765",
+            "98765432109876543", "09876543210987654321"};
 
     @BeforeAll
-    public static void setup() {
-
-        baseURI = "http://35.208.34.242";
-        port = 8080;
+    public static void setup(){
+        
+        baseURI="http://35.208.34.242";
+        port=8080;
 
     }
-
-    @ParameterizedTest
-    @CsvSource({
-            "Vasya, 12345",
-            "Petya, 54321",
-            "Masha, qwerty"
-    })
-    public void loginWithValidUsernameAndPasswordCheckResponseBodyAndResponseCodeIsOk(String username, String password) {
-
-        Response response = given()
-                .header("content-type", "application/json")
-                .log()
-                .all()
-                .when()
-                .get("/test-orders?username={username}&password={password}", username, password)
-                .then()
-                .log()
-                .all()
-                .statusCode(HttpStatus.SC_OK)
-                .and()
-                .extract()
-                .response();
-
-        assertTrue(response.path("message").toString().contains(username));
-        assertEquals(16, response.path("apiKey").toString().length());
-
+    @BeforeEach void eachSetup(){
+        System.out.println("\n+++++++++++++++++++++++++++");
     }
 
     @Test
-    public void loginWithoutUsernameAndPasswordCheckResponseBodyAndResponseCodeIsBadRequest() {
-
-        Response response = given()
-                .header("Content-type", "application/json")
-                .log()
-                .all()
-                .when()
-                .get("/test-orders?username=&password=")
-                .then()
-                .log()
-                .all()
-                .statusCode(HttpStatus.SC_BAD_REQUEST)
-                .and()
-                .extract()
-                .response();
-
-        assertNull(response.path("apiKey"));
-        assertEquals("Username or password is missing", response.path("message"));
-
-    }
-
-    @Test
-    public void getAllOrderAndCheckResponseCodeIsOk() {
+    public void getAllOrderAndCheckResponseCodeIsOk(){
 
         System.out.println("\n===========================");
-        //RestAssured
         given()
                 .log()
                 .all()
@@ -87,383 +37,114 @@ public class RestApiMockedTest {
                 .then()
                 .log()
                 .all()
-                .statusCode(HttpStatus.SC_OK);
-
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 5, 9, 10})
-    public void getOrderByValidIdCheckMatchesInResponseBodyAndResponseCodeIsOk(int validId) {
-
-        System.out.println("\n===========================");
-        Response response = given()
-                .log()
-                .all()
-                .when()
-                .get("/test-orders/{validId}", validId)
-                .then()
-                .log()
-                .all()
-                .statusCode(HttpStatus.SC_OK)
-                .and()
-                .extract()
-                .response();
-
-        var id = response.path("id");
-
-        assertEquals(validId, id);
-        assertEquals("OPEN", response.path("status"));
-
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {-1, 0, 11, 12, 20})
-    public void getOrderByInvalidIdAndCheckResponseCodeIsBadRequest(int invalidId) {
-
-        System.out.println("\n===========================");
-        given()
-                .log()
-                .all()
-                .when()
-                .get("/test-orders/{invalidId}", invalidId)
-                .then()
-                .log()
-                .all()
-                .statusCode(HttpStatus.SC_BAD_REQUEST);
-
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 5, 9, 10})
-    public void deleteOrderIfValidIdAndApiKeyCheckResponseCodeIsOk(int validId) {
-
-        System.out.println("\n===========================");
-        given()
-                .log()
-                .all()
-                .when()
-                .header("api_key", VALID_API_KEY)
-                .delete("/test-orders/{validId}", validId)
-                .then()
-                .log()
-                .all()
-                .statusCode(HttpStatus.SC_NO_CONTENT);
-
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {-1, 0, 11, 12, 20})
-    public void deleteOrderIfInvalidIdAndCheckResponseCodeIsBadRequest(int invalidId) {
-
-        System.out.println("\n===========================");
-        given()
-                .log()
-                .all()
-                .when()
-                .header("api_key", VALID_API_KEY)
-                .delete("/test-orders/{invalidId}", invalidId)
-                .then()
-                .log()
-                .all()
-                .statusCode(HttpStatus.SC_BAD_REQUEST);
-
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"", "3", "67594063", "123456789012345",
-            "qwertyuiopasdfgh", "5w2r4y7i3p1s6f8h", "$&*()_+-=|:<>'`~", "123456789.098765",
-            "98765432109876543", "09876543210987654321"})
-    public void deleteOrderIfInvalidApiKeyAndCheckResponseCodeIsUnauthorized(String invalidApiKey) {
-
-        System.out.println("\n===========================");
-        given()
-                .log()
-                .all()
-                .when()
-                .header("api_key", invalidApiKey)
-                .delete("/test-orders/7")
-                .then()
-                .log()
-                .all()
-                .statusCode(HttpStatus.SC_UNAUTHORIZED);
+                .statusCode(200);
 
     }
 
     @Test
-    public void deleteOrderIfNoHeaderFieldAndCheckResponseCodeIsBadRequest() {
+    public void getOrderByValidIdAndCheckResponseCodeIsOk(){
 
-        System.out.println("\n===========================");
-        given()
-                .log()
-                .all()
-                .when()
-                .delete("/test-orders/7")
-                .then()
-                .log()
-                .all()
-                .statusCode(HttpStatus.SC_BAD_REQUEST);
+        for (int x: VALID_ID){
+            System.out.println("\n===========================");
+            given()
+                    .log()
+                    .all()
+                    .when()
+                    .get("/test-orders/" + x)
+                    .then()
+                    .log()
+                    .all()
+                    .statusCode(200);
 
+        }
     }
-
-//    @Test
-//    public void createAnOrderWithCorrectJsonDataCheckResponseBodyAndResponseCodeIsOk() {
-//
-//        JsonObject body = new JsonObject();
-//        body.addProperty("status", "OPEN");
-//        body.addProperty("courierId", 0);
-//        body.addProperty("customerName", "dm");
-//        body.addProperty("customerPhone", 123456);
-//        body.addProperty("comment", "qwerty");
-//        body.addProperty("id", 0);
-//
-//        Response response = given()
-//                .header("Accept", "application/json")
-//                .header("Content-type", "application/json")
-//                .body(body)
-//                .log()
-//                .all()
-//                .when()
-//                .post("/test-orders")
-//                .then()
-//                .log()
-//                .all()
-//                .statusCode(HttpStatus.SC_OK)
-//                .and()
-//                .extract()
-//                .response();
-//
-//    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {
-
-            "{\"status\": \"OPEN\", \"courierId\": 0, \"customerName\": \"dm\", \"customerPhone\": 123456, \"comment\": \"qwerty\", \"id\": 0}",
-            "{\"status\": \"ACCEPTED\", \"courierId\": 0, \"customerName\": \"dm\", \"customerPhone\": 123456, \"comment\": \"qwerty\", \"id\": 0}",
-            "{\"status\": \"INPROGRESS\", \"courierId\": 0, \"customerName\": \"dm\", \"customerPhone\": 123456, \"comment\": \"qwerty\", \"id\": 0}",
-            "{\"status\": \"DELIVERED\", \"courierId\": 0, \"customerName\": \"dm\", \"customerPhone\": 123456, \"comment\": \"qwerty\", \"id\": 0}",
-
-    })
-    public void createOrderWithCorrectJsonDataCheckMatchesInResponseBodyAndResponseCodeIsOk(String str) {
-
-        var id = given()
-                .header("Accept", "application/json")
-                .header("Content-type", "application/json")
-                .body(str)
-                .log()
-                .all()
-                .when()
-                .post("/test-orders")
-                .then()
-                .log()
-                .all()
-                .statusCode(HttpStatus.SC_OK)
-                .extract()
-                .path("id");
-
-        assertNotEquals(0, id);
-
-    }
-
-    String str = "\"status\": \"OPEN\", \"courierId\": 0, \"customerName\": \"dm\", \"customerPhone\": 123456, \"comment\": \"qwerty\", \"id\": 0";
 
     @Test
-    public void createOrderWithCorrectJsonDataButWithoutHeadersCheckResponseCodeIsBadRequest() {
+    public void getOrderByInvalidIdAndCheckResponseCodeIsBadRequest(){
 
-        System.out.println("\n===========================");
-        given()
-                .body(str)
+        for (int x: INVALID_ID){
+            System.out.println("\n===========================");
+            given()
                 .log()
                 .all()
                 .when()
-                .delete("/test-orders/7")
+                .get("/test-orders/" + x)
                 .then()
                 .log()
                 .all()
-                .statusCode(HttpStatus.SC_BAD_REQUEST);
-
+                .statusCode(400);
+        }
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {
+    @Test
+    public void deleteOrderIfValidIdAndApiKeyCheckResponseCodeIsOk(){
 
-            "{\"status\": 12345, \"courierId\": 0, \"customerName\": \"dm\", \"customerPhone\": 123456, \"comment\": \"qwerty\", \"id\": 0}",
-            "{\"status\": 'F', \"courierId\": 0, \"customerName\": \"dm\", \"customerPhone\": 123456, \"comment\": \"qwerty\", \"id\": 0}",
-            "{\"status\": \"qwerty\", \"courierId\": 0, \"customerName\": \"dm\", \"customerPhone\": 123456, \"comment\": \"qwerty\", \"id\": 0}",
-            "{\"status\": \"OPEN\", \"courierId\": 'f', \"customerName\": \"dm\", \"customerPhone\": 123456, \"comment\": \"qwerty\", \"id\": 0}",
-            "{\"status\": \"OPEN\", \"courierId\": \"qwerty\", \"customerName\": \"dm\", \"customerPhone\": 123456, \"comment\": \"qwerty\", \"id\": 0}",
-            "{\"status\": , \"courierId\": , \"customerName\": , \"customerPhone\": , \"comment\": , \"id\": }",
+        for (int x: VALID_ID){
+            System.out.println("\n===========================");
+            given()
+                    .log()
+                    .all()
+                    .when()
+                    .header("api_key", VALID_API_KEY)
+                    .delete("/test-orders/" + x)
+                    .then()
+                    .log()
+                    .all()
+                    .statusCode(204);
+        }
+    }
 
-    })
-    public void createOrderWithIncorrectJsonDataCheckResponseBodyAndResponseCodeIsBadRequest(String str) {
+    @Test
+    public void deleteOrderIfInvalidIdAndCheckResponseCodeIsBadRequest(){
 
-        Response response = given()
-                .header("Accept", "application/json")
-                .header("Content-type", "application/json")
-                .body(str)
+        for (int x: INVALID_ID){
+            System.out.println("\n===========================");
+            given()
                 .log()
                 .all()
                 .when()
-                .post("/test-orders")
+                .header("api_key", VALID_API_KEY)
+                .delete("/test-orders/" + x)
                 .then()
                 .log()
                 .all()
-                .statusCode(HttpStatus.SC_BAD_REQUEST)
-                .extract()
-                .response();
-
-        assertEquals("Incorrect query", response.asString());
-
+                .statusCode(400);
+        }
     }
 
-    @ParameterizedTest
-    @CsvSource({
-    //Threads, required requests per sec, timer
-            "30, 30, 30",
-            "20, 20, 240"
-    })
-    public void performanceTestOfGetAllOrdersEndpoint(int threadsAmount, int load, int timer)
-            throws InterruptedException {
+    @Test
+    public void deleteOrderIfInvalidApiKeyAndCheckResponseCodeIsUnauthorized(){
 
-        PerformanceTest.threadsAmount = threadsAmount;
-        PerformanceTest.timer = timer * 1000;
-        PerformanceTest.load = load;
-        PerformanceTest.startTime = PerformanceTest.timer;
-        PerformanceTest.caughtCode = 0;
-        PerformanceTest.stop = false;
-
-        PerformanceTest[] thrd = new PerformanceTest[threadsAmount];
-
-        for (int i = 0; i < thrd.length; i++) {
-
-            thrd[i] = PerformanceTest.createAndStart("Thread-" + i);
+        for (String x: INVALID_API_KEY){
+            System.out.println("\n===========================");
+            given()
+                    .log()
+                    .all()
+                    .when()
+                    .header("api_key", x)
+                    .delete("/test-orders/7")
+                    .then()
+                    .log()
+                    .all()
+                    .statusCode(401);
 
         }
-
-        while (!PerformanceTest.stop && PerformanceTest.timer > 0) {
-
-            PerformanceTest.timer -= 1000;
-            Thread.sleep(1000);
-
-            double minLoad = 0;
-            double maxLoad = 0;
-            int minRequestsCount = 0;
-            int maxRequestsCount = 0;
-            int totalRequestsCount = 0;
-
-            for (int i = 0; i < thrd.length; i++) {
-
-                totalRequestsCount += thrd[i].requestsCount;
-
-                if (i == 0) {
-                    minLoad = thrd[i].threadCurrentLoad;
-                    minRequestsCount = thrd[i].requestsCount;
-                }
-                if (maxLoad < thrd[i].threadCurrentLoad) maxLoad = thrd[i].threadCurrentLoad;
-                if (minLoad > thrd[i].threadCurrentLoad) minLoad = thrd[i].threadCurrentLoad;
-
-                if (maxRequestsCount < thrd[i].requestsCount) maxRequestsCount = thrd[i].requestsCount;
-                if (minRequestsCount > thrd[i].requestsCount) minRequestsCount = thrd[i].requestsCount;
-
-            }
-
-            double i = timer - PerformanceTest.timer * 0.001;
-            double s = (double) totalRequestsCount / thrd.length;
-
-            System.out.println("Avg. req/sec: " + String.format("%.3f", totalRequestsCount / i) +
-                    " | Req/sec from each thread(" + thrd.length + ") min-avg-max: " +
-                    String.format("%.3f", minLoad) + " - " + String.format("%.3f", s / i) + " - " + String.format("%.3f", maxLoad) +
-                    " | Total req.: " + totalRequestsCount + " | Time rem.: " + PerformanceTest.timer / 1000);
-
-        }
-
-        Thread.sleep(5000);
-
-        PerformanceTest.clear(thrd);
-
-        assertEquals(200, PerformanceTest.caughtCode);
-
-    }
-}
-
-class PerformanceTest implements Runnable {
-
-    Thread thisThread;
-    static String threadThatStoppedTest;
-    static int timer;
-    static int startTime;
-    static int threadsAmount;
-    static volatile int caughtCode;
-    static int load;
-    static boolean stop;
-    double threadCurrentLoad;
-    int threadStatusCode;
-    int requestsCount;
-    int time;
-
-    PerformanceTest(String name) {
-
-        thisThread = new Thread(this, name);
-
     }
 
-    static void clear(PerformanceTest[] tth) {
+    @Test
+    public void deleteOrderIfNoHeaderFieldAndCheckResponseCodeIsBadRequest(){
 
-        for (PerformanceTest testThread : tth) {
-
-            if (testThread.thisThread.isAlive()) testThread.thisThread.interrupt();
-
-        }
-
-        PerformanceTest.threadThatStoppedTest = "";
-
-    }
-
-    public static PerformanceTest createAndStart(String name) {
-
-        PerformanceTest myThrd = new PerformanceTest(name);
-        myThrd.thisThread.start();
-
-        return myThrd;
-
-    }
-
-    @Override
-    public void run() {
-
-        while (caughtCode != 429 && timer != 0 && !stop) {
-
-            time = startTime - timer;
-
-            threadCurrentLoad = requestsCount / (time * 0.001);
-
-            if (requestsCount / (time * 0.001) <= (double) load / PerformanceTest.threadsAmount) {
-
-                threadStatusCode = get("http://35.208.34.242:8080/test-orders/get_orders")
-                        .statusCode();
-
-                requestsCount++;
+            System.out.println("\n===========================");
+            given()
+                    .log()
+                    .all()
+                    .when()
+                    .delete("/test-orders/7")
+                    .then()
+                    .log()
+                    .all()
+                    .statusCode(400);
 
 
-                if (stop) return;
-
-                else if (threadStatusCode == 429) {
-
-                    caughtCode = threadStatusCode;
-                    threadThatStoppedTest = thisThread.getName();
-                    stop = true;
-                    System.out.println(threadThatStoppedTest + " stopped the test!");
-                    return;
-
-                } else caughtCode = threadStatusCode;
-
-            } else {
-
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
     }
 }
